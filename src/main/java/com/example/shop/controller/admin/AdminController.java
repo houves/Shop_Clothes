1c93f777887320c9dfc5bc78bc5c292e61a2d74e
sangvv343@outlook.com <sangvv343@outlook.com>
2023-06-14 23:34:26 +0700

    four

--- src/main/java/com/example/shop/controller/admin/AdminController.java
+++ src/main/java/com/example/shop/controller/admin/AdminController.java
@@ -1,14 +1,34 @@
 package com.example.shop.controller.admin;
 
 
+import com.example.shop.entity.User;
+import com.example.shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private UserService userService;
     @GetMapping
     public String homeAdmin(){return "admin/index";}
-
+    @GetMapping("/adminUser/listAdminUser")
+    public String showAllUser(Model model){
+        List<User> users = userService.getAllUser();
+        model.addAttribute("users", users);
+        return "/admin/adminUser/listAdminUser";
+    }
+    @GetMapping("/adminUser/deleteAdminUser/{id}")
+    public String deleteClotheAdmin(@PathVariable("id") Long id, Model model){
+        User user = userService.getUserById(id);
+        userService.deteleUser(id);
+        return "redirect:/admin/adminUser/listAdminUser";
+    }
 }
--- src/main/java/com/example/shop/service/UserService.java
+++ src/main/java/com/example/shop/service/UserService.java
@@ -6,14 +6,31 @@ import com.example.shop.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private IUserRepository userRepository;
-    public void save(User user){
-        userRepository.save(user);
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+
+    public User getUserById(Long id){
+        Optional<User> optional = userRepository.findById(id);
+        return optional.orElse(null);
+    }
+    public List<User> getByNameUser(String name){
+        List<User> listTemp = new ArrayList<>();
+        for (User user : getAllUser()){
+            if(user.getName().contains(name)){
+                listTemp.add(user);
+            }
+        }
+        return listTemp;
     }
 
     public List<Cart> getAllCartByUser(Long id_user){
@@ -21,4 +38,8 @@ public class UserService {
         return null;
 
     }
+    public void save(User user){
+        userRepository.save(user);
+    }
+    public void deteleUser(Long id){userRepository.deleteById(id);}
 }
